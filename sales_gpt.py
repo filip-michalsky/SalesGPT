8883c2e719b8d4e3b6f8d3c387c71ad7f3693b5b
Filip Michalsky <filipmichalsky@gmail.com>
2023-05-14 18:19:37 -0400

    update timer

--- sales_gpt.py
+++ sales_gpt.py
@@ -1,12 +1,36 @@
 from typing import Dict, List, Any, Tuple
 from copy import deepcopy
-from base_logger import time_logger
 from langchain import LLMChain, PromptTemplate
 from langchain.llms import BaseLLM
 from pydantic import BaseModel, Field
 from langchain.chains.base import Chain
 from langchain.chat_models import ChatOpenAI
 
+import time
+import logging
+from functools import wraps
+
+logger = logging.getLogger(__name__)
+
+stream_handler = logging.StreamHandler()
+log_filename='output.log'
+file_handler = logging.FileHandler(filename=log_filename)
+handlers = [stream_handler, file_handler]
+
+# Configure the logging module
+logging.basicConfig(level=logging.WARN, format='%(name)s %(asctime)s - %(levelname)s - %(message)s', handlers=handlers)
+
+def time_logger(func):
+    """Decorator function to log time taken by any function."""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()  # Start time before function execution
+        result = func(*args, **kwargs)  # Function execution
+        end_time = time.time()  # End time after function execution
+        execution_time = end_time - start_time  # Calculate execution time
+        logger.warn(f"Running {func.__name__}: --- {execution_time} seconds ---")
+        return result
+    return wrapper
 
 CONVERSATION_STAGES = {'1' : "Introduction: Start the conversation by introducing yourself and your company. Be polite and respectful while keeping the tone of the conversation professional. Your greeting should be welcoming. Always clarify in your greeting the reason why you are calling.",
 '2': "Qualification: Qualify the prospect by confirming if they are the right person to talk to regarding your product/service. Ensure that they have the authority to make purchasing decisions.",
