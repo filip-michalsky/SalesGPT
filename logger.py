3d75941c9c51cc620545ebc816c7b469cabb82de
Filip Michalsky <filipmichalsky@gmail.com>
2023-05-15 11:05:03 -0400

    improve time logger, add filtering for logs.

--- logger.py
+++ logger.py
@@ -9,8 +9,14 @@ log_filename='output.log'
 file_handler = logging.FileHandler(filename=log_filename)
 handlers = [stream_handler, file_handler]
 
+class TimeFilter(logging.Filter):
+    def filter(self, record):
+        return 'Running' in record.getMessage()
+
+logger.addFilter(TimeFilter())
+
 # Configure the logging module
-logging.basicConfig(level=logging.WARN, format='%(name)s %(asctime)s - %(levelname)s - %(message)s', handlers=handlers)
+logging.basicConfig(level=logging.INFO, format='%(name)s %(asctime)s - %(levelname)s - %(message)s', handlers=handlers)
 
 def time_logger(func):
     """Decorator function to log time taken by any function."""
@@ -20,6 +26,8 @@ def time_logger(func):
         result = func(*args, **kwargs)  # Function execution
         end_time = time.time()  # End time after function execution
         execution_time = end_time - start_time  # Calculate execution time
-        logger.warn(f"Running {func.__name__}: --- {execution_time} seconds ---")
+        logger.info(f"Running {func.__name__}: --- {execution_time} seconds ---")
         return result
     return wrapper
+
+
--- sales_gpt.py
+++ sales_gpt.py
@@ -6,31 +6,11 @@ from pydantic import BaseModel, Field
 from langchain.chains.base import Chain
 from langchain.chat_models import ChatOpenAI
 
-import time
-import logging
-from functools import wraps
+import os, sys
+DIRNAME = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(DIRNAME)
+from logger import time_logger
 
-logger = logging.getLogger(__name__)
-
-stream_handler = logging.StreamHandler()
-log_filename='output.log'
-file_handler = logging.FileHandler(filename=log_filename)
-handlers = [stream_handler, file_handler]
-
-# Configure the logging module
-logging.basicConfig(level=logging.WARN, format='%(name)s %(asctime)s - %(levelname)s - %(message)s', handlers=handlers)
-
-def time_logger(func):
-    """Decorator function to log time taken by any function."""
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start_time = time.time()  # Start time before function execution
-        result = func(*args, **kwargs)  # Function execution
-        end_time = time.time()  # End time after function execution
-        execution_time = end_time - start_time  # Calculate execution time
-        logger.warn(f"Running {func.__name__}: --- {execution_time} seconds ---")
-        return result
-    return wrapper
 
 CONVERSATION_STAGES = {'1' : "Introduction: Start the conversation by introducing yourself and your company. Be polite and respectful while keeping the tone of the conversation professional. Your greeting should be welcoming. Always clarify in your greeting the reason why you are calling.",
 '2': "Qualification: Qualify the prospect by confirming if they are the right person to talk to regarding your product/service. Ensure that they have the authority to make purchasing decisions.",
