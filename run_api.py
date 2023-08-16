cc1a152ffec476a01ffb140b952c28450c34a23c
Jane wu <conchwu@hotmail.com>
2023-08-16 17:13:53 +0800

    add: api mode

---
+++ run_api.py
@@ -0,0 +1,48 @@
+import os
+from typing import List
+
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from salesgpt.salesgptapi import SalesGPTAPI
+
+app = FastAPI()
+
+GPT_MODEL = "gpt-3.5-turbo-0613"
+# GPT_MODEL_16K = "gpt-3.5-turbo-16k-0613"
+
+
+@app.get("/")
+async def say_hello():
+    return {"message": "Hello World"}
+
+
+class MessageList(BaseModel):
+    conversation_history: List[str]
+    human_say: str
+
+
+@app.post("/chat")
+async def chat_with_sales_agent(req: MessageList):
+    sales_api = SalesGPTAPI(config_path='examples/example_agent_setup.json', verbose=True)
+    name, reply = sales_api.do(req.conversation_history, req.human_say)
+    res = {
+        "name": name,
+        "say": reply
+    }
+    return res
+
+
+def _set_env():
+    with open('.env', 'r') as f:
+        env_file = f.readlines()
+    envs_dict = {key.strip("'"): value.strip("\n") for key, value in [(i.split('=')) for i in env_file]}
+    os.environ['OPENAI_API_KEY'] = envs_dict['OPENAI_API_KEY']
+
+
+if __name__ == '__main__':
+    _set_env()
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+
---
+++ salesgpt/salesgptapi.py
@@ -0,0 +1,70 @@
+import json
+
+from langchain.chat_models import ChatOpenAI
+
+from salesgpt.agents import SalesGPT
+
+GPT_MODEL = "gpt-3.5-turbo-0613"
+# GPT_MODEL_16K = "gpt-3.5-turbo-16k-0613"
+
+
+class SalesGPTAPI:
+    USE_TOOLS = False
+
+    def __init__(self, config_path: str, verbose: bool = False, max_num_turns: int = 10):
+        self.config_path = config_path
+        self.verbose = verbose
+        self.max_num_turns = max_num_turns
+        self.llm = ChatOpenAI(temperature=0.2, model_name=GPT_MODEL)
+
+    def do(self, conversation_history: [str], human_input=None):
+    
+        if self.config_path == '':
+            print('No agent config specified, using a standard config')
+            # USE_TOOLS = True
+            if self.USE_TOOLS:
+                sales_agent = SalesGPT.from_llm(self.llm, use_tools=True,
+                                                product_catalog="examples/sample_product_catalog.txt",
+                                                salesperson_name="Ted Lasso",
+                                                verbose=self.verbose)
+            else:
+                sales_agent = SalesGPT.from_llm(self.llm, verbose=self.verbose)
+
+        else:
+            with open(self.config_path, 'r') as f:
+                config = json.load(f)
+            if self.verbose:
+                print(f'Agent config {config}')
+            sales_agent = SalesGPT.from_llm(self.llm, verbose=self.verbose, **config)
+
+        #  check turns
+        current_turns = len(conversation_history) + 1
+        if current_turns >= self.max_num_turns:
+            # todo:
+            # if self.verbose:
+            print('Maximum number of turns reached - ending the conversation.')
+            return "<END_OF_>"
+
+        # seed
+        sales_agent.seed_agent()
+        sales_agent.conversation_history = conversation_history
+
+        if human_input is not None:
+            sales_agent.human_step(human_input)
+            # sales_agent.determine_conversation_stage()
+            # print('=' * 10)
+            # print(f"conversation_stage_id:{sales_agent.conversation_stage_id}")
+            
+        sales_agent.step()
+
+        # end conversation
+        if '<END_OF_CALL>' in sales_agent.conversation_history[-1]:
+            print('Sales Agent determined it is time to end the conversation.')
+            return "<END_OF_CALL>"
+
+        reply = sales_agent.conversation_history[-1]
+
+        if self.verbose:
+            print('=' * 10)
+            print(f"{sales_agent.salesperson_name}:{reply}")
+        return reply.split(": ")
