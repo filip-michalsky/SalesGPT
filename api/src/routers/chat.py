72cda9a5b9c8b89db7d1ab3def730a49255d8a53
Evgeniy Ermakov <erma2001@mail.ru>
2024-02-18 19:43:25 +0700

    изменения в роутере (работа с redis)

--- api/src/routers/chat.py
+++ api/src/routers/chat.py
@@ -2,23 +2,16 @@ from fastapi import APIRouter
 
 from api.redis.redis import Redis
 from api.schemas.chatDTO import Chat
-from redis.commands.json.path import Path
-import json
 
 router = APIRouter()
 
 redis = Redis()
 
-@router.post("/save")
-async def save_messages(telegram_id: str, messages: list[str]):
-
+@router.post("")
+async def save_messages(telegram_id: str):
     chat_session = Chat(
-        telegram_id=telegram_id,
         messages=[],
     )
-    print(chat_session)
     async with redis:
-        await redis.set(key = str(telegram_id), value = chat_session.model_dump())
-        
-    
-    return {"status": 200, "telegram_id": telegram_id}
+        await redis.set(name = str(telegram_id), obj = chat_session.model_dump(), ex=3600)
+    return {"telegram_id": telegram_id}
