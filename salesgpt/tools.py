07cae3d37ef0f7d53ad9efedb121aa95e781ea03
Filip Michalsky <filip_michalsky@g.harvard.edu>
2024-03-25 15:42:09 -0700

    update example + tools.py

--- salesgpt/tools.py
+++ salesgpt/tools.py
@@ -7,6 +7,7 @@ from langchain.chains import RetrievalQA
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from litellm import completion
 
 
 def setup_knowledge_base(
@@ -34,16 +35,72 @@ def setup_knowledge_base(
     return knowledge_base
 
 
+def get_product_id_from_query(query, product_price_id_mapping_path):
+    # Load product_price_id_mapping from a JSON file
+    with open(product_price_id_mapping_path, 'r') as f:
+        product_price_id_mapping = json.load(f)
+    
+    # Serialize the product_price_id_mapping to a JSON string for inclusion in the prompt
+    product_price_id_mapping_json_str = json.dumps(product_price_id_mapping)
+    
+    # Dynamically create the enum list from product_price_id_mapping keys
+    enum_list = list(product_price_id_mapping.values()) + ["No relevant product id found"]
+    enum_list_str = json.dumps(enum_list)
+    
+    prompt = f"""
+    You are an expert data scientist and you are working on a project to recommend products to customers based on their needs.
+    Given the following query:
+    {query}
+    and the following product price id mapping:
+    {product_price_id_mapping_json_str}
+    return the price id that is most relevant to the query.
+    ONLY return the price id, no other text. If no relevant price id is found, return 'No relevant price id found'.
+    Your output will follow this schema:
+    {{
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "title": "Price ID Response",
+    "type": "object",
+    "properties": {{
+        "price_id": {{
+        "type": "string",
+        "enum": {enum_list_str}
+        }}
+    }},
+    "required": ["price_id"]
+    }}
+    Return a valid directly parsable json, dont return in it within a code snippet or add any kind of explanation!!
+    """
+    prompt+='{'
+    response = completion(
+        model=os.getenv("GPT_MODEL", "gpt-3.5-turbo-1106"),
+        messages=[{"content": prompt, "role": "user"}],
+        max_tokens=1000,
+        temperature=0
+    )
+
+    product_id = response.choices[0].message.content.strip()
+    return product_id
+
+
 def generate_stripe_payment_link(query: str) -> str:
     """Generate a stripe payment link for a customer based on a single query string."""
 
-    url = os.getenv("MINDWARE_URL", "")
-    api_key = os.getenv("MINDWARE_API_KEY", "")
-
-    payload = json.dumps({"prompt": query})
-    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
-
-    response = requests.request("POST", url, headers=headers, data=payload)
+    # example testing payment gateway url
+    PAYMENT_GATEWAY_URL = os.getenv("PAYMENT_GATEWAY_URL", "https://agent-payments-gateway.vercel.app/payment")
+    PRODUCT_PRICE_MAPPING = "example_product_price_id_mapping.json"
+    
+    # use LLM to get the price_id from query
+    price_id = get_product_id_from_query(query, PRODUCT_PRICE_MAPPING)
+    price_id = json.loads(price_id)
+    payload = json.dumps({"prompt": query,
+                          **price_id,
+                          'stripe_key': os.getenv("STRIPE_API_KEY")
+                          })
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    
+    response = requests.request("POST", PAYMENT_GATEWAY_URL, headers=headers, data=payload)
     return response.text
 
 
