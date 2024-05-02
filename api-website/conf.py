a8c1de9713ea8fc5fe4e97da7485a378984810f9
chemik-bit <honza.michna.00@gmail.com>
2024-05-02 16:43:04 +0200

    web improvements v6

--- api-website/conf.py
+++ api-website/conf.py
@@ -40,8 +40,8 @@ extensions = ['sphinx.ext.autodoc',
 
 GOOGLE_TAG_MANAGER_SNIPPET = """
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({
-'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NX3SZD79');</script>
