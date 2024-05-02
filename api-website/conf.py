9ca2a4b1fe0a8ad7a3645b5f9927b685f7d81d60
chemik-bit <honza.michna.00@gmail.com>
2024-05-02 16:17:31 +0200

    web improvements v3

--- api-website/conf.py
+++ api-website/conf.py
@@ -44,10 +44,9 @@ GOOGLE_TAG_MANAGER_SNIPPET = f"""
 'gtm.start': new Date().getTime(),event:'gtm.js'}});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-}})(window,document,'script','dataLayer',{GTAG_API});</script>
+}})(window,document,'script','dataLayer','{GTAG_API}');</script>
 <!-- End Google Tag Manager -->
 """
-
 
 # 2. Create a setup() function if you don't already have one.
 #    (If you do, just add to your existing setup() function.)
