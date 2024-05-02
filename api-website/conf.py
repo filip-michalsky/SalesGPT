3464f39a478bd7fcd68acbe6952e72de449dd2d1
chemik-bit <honza.michna.00@gmail.com>
2024-05-02 17:39:58 +0200

    web improvements v10.5

--- api-website/conf.py
+++ api-website/conf.py
@@ -38,15 +38,13 @@ extensions = ['sphinx.ext.autodoc',
               #'sphinxcontrib.gtagjs'
 ]
 GTAG_JS = """
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VH1ZNBVHSP"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-VH1ZNBVHSP');
-</script>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NX3SZD79');</script>
+<!-- End Google Tag Manager -->
 """
 googleanalytics_id = "G-VH1ZNBVHSP"
 
