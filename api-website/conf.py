4d944c577a5c0acbfd44091c1d04377716c29c27
chemik-bit <honza.michna.00@gmail.com>
2024-05-02 17:23:53 +0200

    web improvements v10.3

--- api-website/conf.py
+++ api-website/conf.py
@@ -34,15 +34,15 @@ author = "Filip-Michalsky"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              "sphinxcontrib.googleanalytics",
+              'sphinxcontrib.googleanalytics',
               #'sphinxcontrib.gtagjs'
 ]
 
-googleanalytics_id = "GTM-NX3SZD79"
+googleanalytics_id = "GTM-TTQZ3ZJ9"
 """
 def setup(app):
     
-    app.add_javascript("https://www.googletagmanager.com/gtag/js?id=GTM-NX3SZD79")
+    app.add_javascript("https://www.googletagmanager.com/gtag/js?id=GTM-TTQZ3ZJ9")
     app.add_javascript("google_analytics_tracker.js")
 """
 templates_path = ['_templates']
